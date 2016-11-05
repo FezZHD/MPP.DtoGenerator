3b4f1e20d0eb1ad2891a9fcf685660c20931976a
FezZHD <fezzdev@gmail.com>
2016-11-05 20:24:49 +0300

    creating project, implementing type interface

---
+++ DtoGenerator/TypeInterface/IType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TypeInterface
+{
+    public interface IType
+    {
+        string Name { get; set; }
+        string Format { get; set; }
+        Type Type { get; set; }
+    }
+}
