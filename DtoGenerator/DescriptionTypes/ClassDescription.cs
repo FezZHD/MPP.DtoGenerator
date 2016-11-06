ea1be3420ddafb7ad18c06b015e12cda72a8746b
FezZHD <fezzdev@gmail.com>
2016-11-06 03:26:35 +0300

    implementing class description struct

---
+++ DtoGenerator/DescriptionTypes/ClassDescription.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DtoGenerator.DescriptionTypes
+{
+    internal class ClassDescription
+    {
+        [JsonProperty("className")]
+        internal string ClassName { get; set; }
+        [JsonProperty("properties")]
+        internal List<PropertyDescription> Proprties { get; set; }
+
+    }
+}
---
+++ DtoGenerator/DescriptionTypes/PropertyDescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using TypeInterface;
+
+namespace DtoGenerator.DescriptionTypes
+{
+    internal class PropertyDescription: IType
+    {
+        [JsonProperty("name")]
+        public string Name { get; }
+        [JsonProperty("format")]
+        public string Format { get; }
+        [JsonProperty("type")]
+        public Type Type { get; }
+
+        internal PropertyDescription(string name, string format, Type type)
+        {
+            Name = name;
+            Format = format;
+            Type = type;
+        }
+    }
+}
